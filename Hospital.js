ba12f071b59cbcef407ec55c078a2515986fbfec
Yash Wadpalliwar <yashhardly16@gmail.com>
2023-11-17 10:44:18 +0530

    first commit

---
+++ Hospital.js
@@ -0,0 +1,50 @@
+//Counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step)
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+
+});
+
+// Initialize Swiper
+
+var swiper = new Swiper( '.swiper-container.app-screen', {
+    effect: 'coverflow',
+    loop: true,
+    autoplaySpeed: 3000,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    paginationClickable: true,
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    }
+});
